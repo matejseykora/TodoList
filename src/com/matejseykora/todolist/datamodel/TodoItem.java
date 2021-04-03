1c1b41f939506bcc765aa13129cad26d16422147
matejseykora <matej.seykora@gmail.com>
2021-04-03 17:21:43 +0200

    set lombok in project settings and accordingly did some cleaning in TodoItem class

--- src/com/matejseykora/todolist/datamodel/TodoItem.java
+++ src/com/matejseykora/todolist/datamodel/TodoItem.java
@@ -1,44 +1,16 @@
 package com.matejseykora.todolist.datamodel;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDate;
 
+@Getter
+@Setter
+@AllArgsConstructor
 public class TodoItem {
     private String shortDescription;
     private String details;
     private LocalDate deadline;
-
-    public TodoItem(String shortDescription, String details, LocalDate deadline) {
-        this.shortDescription = shortDescription;
-        this.details = details;
-        this.deadline = deadline;
-    }
-
-    public String getShortDescription() {
-        return shortDescription;
-    }
-
-    public void setShortDescription(String shortDescription) {
-        this.shortDescription = shortDescription;
-    }
-
-    public String getDetails() {
-        return details;
-    }
-
-    public void setDetails(String details) {
-        this.details = details;
-    }
-
-    public LocalDate getDeadline() {
-        return deadline;
-    }
-
-    public void setDeadline(LocalDate deadline) {
-        this.deadline = deadline;
-    }
-
-//    @Override
-//    public String toString() {
-//        return shortDescription;
-//    }
 }
--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,7 @@
 module TodoList {
     requires javafx.fxml;
     requires javafx.controls;
+    requires lombok;
 
     opens com.matejseykora.todolist;
 }
